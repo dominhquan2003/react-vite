2c24397abc90569ce57024a9bdbcfa714700c0f7
dominhquan2003 <doquan23032003@gmail.com>
2024-11-11 13:25:17 +0700

    loop in jsx,Variable with jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,12 +3,32 @@
 // jsx => chi tra ve 1 thang parent duy nhat
 import './style.css';
 const MyComponent = () => {
+      const bin = " Do Minh Quan "; // string 
+      const objectToSting = { name: "Do Minh Quan", age: 20 }; // object
+      const nestedObject = {
+            person1: { name: "Alice", age: 25 },
+            person2: { name: "Bob", age: 30 },
+            person3: { name: "Charlie", age: 35 }
+      };
       return (
             <>
-                  <div>  My Do Minh Quan Component </div>
+                  <div>  {JSON.stringify(objectToSting)} || {objectToSting.name} My Component </div>
+                  {console.log(Object.keys(nestedObject))}
+                  {/* use loop with nest object in jsx */}
+                  <ul>
+                        {Object.keys(nestedObject).map((person, index) => {
+                              return (
+                                   
+                                    <li key={index}>
+                                          <p>{nestedObject[person].name}</p>
+                                          <p>{nestedObject[person].age}</p>
+                                    </li>
+                              );
+                        })} 
+                  </ul>
                   <div className="child"
-                        style={{ borderRadius : "10px", borderColor:"red" }}>
-                        child 
+                        style={{ borderRadius: "10px", borderColor: "red" }}>
+                        child  {bin}
                   </div>
             </>
       );
