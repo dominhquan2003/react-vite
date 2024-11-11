e59dcaaff3de05c7e6071fb3d7cccc4fe67a26c0
dominhquan2003 <doquan23032003@gmail.com>
2024-11-11 15:10:27 +0700

    todo component

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,9 @@
+const ToDoData = () => {
+      return (
+            <div className='todo-data'>
+                  <div>Learning React </div>
+                  <div>MERN STACK</div>
+            </div>
+      )
+}
+export default ToDoData
---
+++ src/components/todo/TodoImg.jsx
@@ -0,0 +1,9 @@
+import reactLogo from '../../assets/react.svg'
+const ToDoImg = () => {
+      return (
+            <div className='todo-image'>
+                  <img src={reactLogo} className='logo' alt="" />
+            </div>
+      )
+}
+export default ToDoImg
