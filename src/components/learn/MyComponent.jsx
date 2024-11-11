132c33815cf79dd6752b3dd5fbba81be260125e7
dominhquan2003 <doquan23032003@gmail.com>
2024-11-11 12:59:04 +0700

    jsx,fragment,link css

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,16 @@
 // Rule Component name must be in PascalCase, myComponent -> MyComponent
 // Component = html + css + js
-
+// jsx => chi tra ve 1 thang parent duy nhat
+import './style.css';
 const MyComponent = () => {
       return (
-            <div>
-                  <h1>My Component</h1>
-            </div>
+            <>
+                  <div>  My Do Minh Quan Component </div>
+                  <div className="child"
+                        style={{ borderRadius : "10px", borderColor:"red" }}>
+                        child 
+                  </div>
+            </>
       );
 }
 
