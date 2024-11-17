c9e15b534c8e511cb0e0962508d3ef08f7206786
dominhquan2003 <doquan23032003@gmail.com>
2024-11-17 16:10:41 +0700

    props

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import TodoNew from './components/todo/TodoNew'
 import TodoImg from './components/todo/TodoImg'
 
 const App = () => {
+  const todoObject = {
+    title: "Learn React",
+    description: "Study the basics of React and build a simple project",
+    completed: false
+  }
+  const name = "Do Minh Quan" ;
+  const age = 20; 
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData  
+        title={todoObject}
+        name={name}
+        age={age}
+      />
       <TodoImg />
     </div>
   )
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,12 @@
-const ToDoData = () => {
+const ToDoData = (props) => {
+      //props is an object that contains all the properties passed to the component
+      console.log(props);
+      const{name,title,age} = props;
       return (
             <div className='todo-data'>
-                  <div>Learning React </div>
+                  <div>{name} {age}</div>
+                  <div>Learning React {title.name} </div>
+                  <span>{title.description}</span>
                   <div>MERN STACK</div>
             </div>
       )
